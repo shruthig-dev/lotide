8c78cb319ea4b7e8676d850ae1f41e016b85b1db
vagrant <shruthigangadharblr@gmail.com>
2020-08-18 15:13:38 +0000

    added tail.js file and modified head and assertEqual.js files

--- assertEqual.js
+++ assertEqual.js
@@ -1,14 +1,14 @@
 const assertEqual = function(actual, expected) {
 
-    if (actual === expected) {
-      console.log(`✅✅✅  Assertion Passed: ${actual}  === ${expected}`);
-    } else {
-      console.log(`🔴🔴🔴  Assertion Failed: ${actual}!== ${expected}`);
-    }
+  if (actual === expected) {
+    console.log(`✅✅✅  Assertion Passed: ${actual}  === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴  Assertion Failed: ${actual}!== ${expected}`);
+  }
   
-  };
+};
   
   
-  assertEqual("Lighthouse Labs", "Bootcamp");
-  assertEqual(1, 1);
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
   
--- head.js
+++ head.js
@@ -1,14 +1,19 @@
 const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅  Assertion Passed : ${actual}  === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴  Assertion Failed : ${actual}!== ${expected}`);
+  }
+};
+  
+const tail = function(array) {
+ // let headOfArray = array[0];
+  //return headOfArray;
 
-    if (actual === expected) {
-      console.log(`✅✅✅  Assertion Passed: ${actual}  === ${expected}`);
-    } else {
-      console.log(`🔴🔴🔴  Assertion Failed: ${actual}!== ${expected}`);
-    }
+  _.tail([1, 2, 3]);
+};
   
-  };
-  
-  
-  assertEqual("Lighthouse Labs", "Bootcamp");
-  assertEqual(1, 1);
-  
+assertEqual(tail([5,6,7]), 5);
+assertEqual(tail(["Hello", "Lighthouse", "Labs"]),"Hello");
+assertEqual(tail([7]), 7);
+assertEqual(tail([]), undefined);
